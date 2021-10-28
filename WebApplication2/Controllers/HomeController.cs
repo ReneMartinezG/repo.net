33ddf388a14b2b6263c064d1c6a5d080c980523b
ReneMartinezG <renemartinezgranados98@gmail.com>
2021-10-28 17:58:24 -0500

    first commit

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+using WebApplication2.Models.ViewModel;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(TablaRegistroViewModel model)
+        {
+            try {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                using (chevezEntities db = new chevezEntities())
+                {
+                    var oRegister = new Register();
+                    oRegister.nombre = model.nombre;
+                    oRegister.email = model.email;
+                    oRegister.conpania = model.conpania;
+                    oRegister.contacto = model.contacto;
+                    oRegister.celular = model.celular;
+                    oRegister.pass = model.pass;
+
+                    db.Register.Add(oRegister);
+                    db.SaveChanges();
+                }
+
+                return Redirect(Url.Content("~/Home/Index"));
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+
+
+            
+
+            return Redirect(Url.Content("~/Home/Index"));
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Models/ViewModel/TablaRegistroViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models.ViewModel
+{
+    public class TablaRegistroViewModel
+    {
+        public int id { get; set; }
+        [Required]
+        [Display(Name ="Nombre")]
+        public string nombre { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "correo electronico")]
+        public string email { get; set; }
+        [Required]
+        [Display(Name = "Compañia")]
+        public string conpania { get; set; }
+        [Required]
+        [Display(Name = "Contacto")]
+        public string contacto { get; set; }
+        [Required]
+        [Display(Name = "Celular")]
+        public string celular { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string pass { get; set; }
+
+    }
+}
